9add5ef6c97f90dfce6b1144989c073f04e1323b
JNieswand <jacob.nieswand@web.de>
2021-01-24 13:04:52 +0100

    Finished everything besides pawn promotion, castle, en passant

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pygame
 import numpy as np
+from enum import Enum  
+
+class Color(Enum):
+    white = 1
+    black = 2
+ 
+def getOpponentColor(color):
+    if color == Color.white:
+        return Color.black
+    elif color == Color.black:
+        return Color.white
+    return None
 
 # game field coordinate ( (0,0) ..-> (8,8))
 class Coordinate():
@@ -20,7 +32,7 @@ class PixPos():
         self.x = x
         self.y = y
         
-def isBishopMove(startcoord, endcoord, pieces):
+def isBishopMove(startcoord, endcoord, game):
     
     move_x = (endcoord.x - startcoord.x) 
     move_y = (endcoord.y - startcoord.y) 
@@ -38,25 +50,22 @@ def isBishopMove(startcoord, endcoord, pieces):
     elif move_y < 0:
         yrange = np.arange(endcoord.y + 1, startcoord.y)
         
-    for piece in pieces:
-        for x in xrange:
-            for y in yrange:
-                coord = Coordinate(x,y)
-                if coord.equals(startcoord):
-                    continue
-                move_x = (coord.x - startcoord.x) 
-                move_y = (coord.y - startcoord.y) 
-                if not abs(move_x) == abs(move_y):
-                    continue
-                print(x, y)
-                if piece.currentCoordinate.equals(Coordinate(x,y)):
-                    print(piece)
-                    return False
-    
-    
-    return 
 
-def isRookMove(startcoord, endcoord, pieces):
+    for x in xrange:
+        for y in yrange:
+            coord = Coordinate(x,y)
+            if coord.equals(startcoord):
+                continue
+            move_x = (coord.x - startcoord.x) 
+            move_y = (coord.y - startcoord.y) 
+            if not abs(move_x) == abs(move_y):
+                continue
+            if game.getPiece(coord):
+                return False
+        
+    return  True
+
+def isRookMove(startcoord, endcoord, game):
     
     move_x = (endcoord.x - startcoord.x) 
     move_y = (endcoord.y - startcoord.y) 
@@ -79,14 +88,13 @@ def isRookMove(startcoord, endcoord, pieces):
         xrange = np.arange(startcoord.x, startcoord.x + 1)
         yrange = np.arange(endcoord.y + 1, startcoord.y)
         
-    for piece in pieces:
-        for x in xrange:
-            for y in yrange:
-                if Coordinate(x,y).equals(startcoord):
-                    continue
-                if piece.currentCoordinate.equals(Coordinate(x,y)):
-                    print(piece)
-                    return False
+   
+    for x in xrange:
+        for y in yrange:
+            if Coordinate(x,y).equals(startcoord):
+                continue
+            if game.getPiece(Coordinate(x,y)):
+                return False
     return True
             
 
@@ -102,24 +110,64 @@ class Piece(pygame.sprite.Sprite):
     def moveVisually(self, new_pos):
         self.rect.center = new_pos
         
-    def isLegalMove(self, coord, otherPieces):
-        for otherPiece in otherPieces:
-            if self is otherPiece:
-                continue
-            if self.team_color == otherPiece.team_color:
-                if coord.equals(otherPiece.currentCoordinate):
-                    return False
+    def isLegalMove(self, coord, game):
+        otherPiece = game.getPiece(coord, self.team_color)
+        if self is otherPiece:
+            return True
+        if otherPiece:
+            return False
         move_x = (coord.x - self.currentCoordinate.x) 
         move_y = (coord.y - self.currentCoordinate.y)         
         
         return abs(move_x)+abs(move_y) !=0
 
+def initPiecesOnBoard():
+    pieces = []
+    for i in np.arange(8):
+        pieces.append(wPawn(Coordinate(1,i), board))
+    pieces.append(wRook(Coordinate(0,0), board))
+    pieces.append(wRook(Coordinate(0,7), board))
+    pieces.append(wKnight(Coordinate(0,1), board))
+    pieces.append(wKnight(Coordinate(0,6), board))
+    pieces.append(wBishop(Coordinate(0,2), board))
+    pieces.append(wBishop(Coordinate(0,5), board))
+    pieces.append(wQueen(Coordinate(0,3), board))
+    pieces.append(wKing(Coordinate(0,4), board))
+    
+    for i in np.arange(8):
+        pieces.append(bPawn(Coordinate(6,i), board))
+    pieces.append(bRook(Coordinate(7,0), board))
+    pieces.append(bRook(Coordinate(7,7), board))
+    pieces.append(bKnight(Coordinate(7,1), board))
+    pieces.append(bKnight(Coordinate(7,6), board))
+    pieces.append(bBishop(Coordinate(7,2), board))
+    pieces.append(bBishop(Coordinate(7,5), board))
+    pieces.append(bQueen(Coordinate(7,3), board))
+    pieces.append(bKing(Coordinate(7,4), board))
+    return pieces
 
-def move(piece, pieces, targetCoordinate):
-        if(piece.isLegalMove(targetCoordinate, pieces)):
-            piece.currentCoordinate = targetCoordinate
-        piece.rect.center = board.getPixPosition(piece.currentCoordinate)
-
+class Game():
+    def __init__(self):
+        self.pieces = initPiecesOnBoard()
+        
+        self.whosTurnIsIt = Color.white
+    def move(self, piece, targetCoordinate):
+            if(piece.isLegalMove(targetCoordinate, self) and 
+               self.whosTurnIsIt == piece.team_color):
+                piece.currentCoordinate = targetCoordinate
+                opponentPiece = self.getPiece(targetCoordinate, getOpponentColor(piece.team_color))
+                if(opponentPiece):
+                    self.pieces.remove(opponentPiece)
+                self.whosTurnIsIt = getOpponentColor(self.whosTurnIsIt)
+            piece.rect.center = board.getPixPosition(piece.currentCoordinate)
+        
+    def getPiece(self, coord, color = None):
+        for piece in self.pieces:
+                if (color != None and color != piece.team_color):
+                    continue
+                if coord.equals(piece.currentCoordinate):
+                    return piece
+        return None
 
 class Board():
     def __init__(self, size):
@@ -142,42 +190,55 @@ class Board():
 class Pawn(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
-    def isLegalMove(self, coord, pieces):
+    def isLegalMove(self, coord, game):
         print("Target field:")
         print(coord.name())
         print("Starting")
         print(self.currentCoordinate.name())
-        legal = super().isLegalMove(coord, pieces)
+        print(self.currentCoordinate.x)
+        legal = super().isLegalMove(coord, game)
         if not legal:
             return False
         
-        move_x = (coord.x - self.currentCoordinate.x) 
-        move_y = (coord.y - self.currentCoordinate.y) 
-        print(move_x)
-        print(self.pawnMoveDirection())
-        if int(move_x) != int(self.pawnMoveDirection()):
-            return False
-        if move_y == 0:
+        if self.isDoublePawnMove(coord, game) or self.isSinglePawnMove(coord, game):
             return True
-        elif abs(move_x) == 1:
-            for otherPiece in pieces:
-                if self.team_color != otherPiece.team_color:
-                    if coord.equals(otherPiece.currentCoordinate):
-                        return True
-        
         return False
     def pawnMoveDirection(self):
         raise(NotImplementedError)
     def startingRow(self):
         raise(NotImplementedError)
+        
+    def isDoublePawnMove(self, coord, game):
+        move_x = (coord.x - self.currentCoordinate.x) 
+        move_y = (coord.y - self.currentCoordinate.y) 
+        print("Move direction (x/y):")
+        print(move_x)
+        print(move_y)
+        if (int(move_x) == 2 * int(self.pawnMoveDirection()) and
+            (self.startingRow() == self.currentCoordinate.x) and 
+            move_y == 0 and 
+            not game.getPiece(coord) and
+            not game.getPiece(Coordinate(self.currentCoordinate.x + self.pawnMoveDirection(), coord.y))):
+            return True
+        
+    def isSinglePawnMove(self, coord, game):
+        move_x = (coord.x - self.currentCoordinate.x) 
+        move_y = (coord.y - self.currentCoordinate.y) 
+        if int(move_x) != int(self.pawnMoveDirection()):
+            return False
+        if move_y == 0 and not game.getPiece(coord):
+            return True
+        elif abs(move_y) == 1:
+            if(game.getPiece(coord, getOpponentColor(self.team_color))):
+               return True    
             
 
 class King(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         
-    def isLegalMove(self, coord, pieces):
-        legal = super().isLegalMove(coord, pieces)
+    def isLegalMove(self, coord, game):
+        legal = super().isLegalMove(coord, game)
         if not legal:
             return False
         move_x = (coord.x - self.currentCoordinate.x) 
@@ -192,13 +253,13 @@ class Queen(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
     
-    def isLegalMove(self, coord, pieces):
-        legal = super().isLegalMove(coord, pieces)
+    def isLegalMove(self, coord, game):
+        legal = super().isLegalMove(coord, game)
         if not legal:
             return False
 
-        if (isBishopMove(self.currentCoordinate, coord, pieces) or 
-            isRookMove(self.currentCoordinate, coord, pieces)):
+        if (isBishopMove(self.currentCoordinate, coord, game) or 
+            isRookMove(self.currentCoordinate, coord, game)):
             return True 
         
         return False
@@ -206,13 +267,13 @@ class Queen(Piece):
 class Rook(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
-    def isLegalMove(self, coord, pieces):
+    def isLegalMove(self, coord, game):
         print(coord.x, coord.y)
-        legal = super().isLegalMove(coord, pieces)
+        legal = super().isLegalMove(coord, game)
         if not legal:
             return False
        
-        if isRookMove(self.currentCoordinate, coord, pieces):
+        if isRookMove(self.currentCoordinate, coord, game):
             return True 
         
         return False
@@ -221,12 +282,12 @@ class Bishop(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         
-    def isLegalMove(self, coord, pieces):
-        legal = super().isLegalMove(coord, pieces)
+    def isLegalMove(self, coord, game):
+        legal = super().isLegalMove(coord, game)
         if not legal:
             return False
 
-        if isBishopMove(self.currentCoordinate, coord, pieces):
+        if isBishopMove(self.currentCoordinate, coord, game):
             return True 
         
         return False
@@ -235,8 +296,8 @@ class Knight(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         
-    def isLegalMove(self, coord, pieces):
-        legal = super().isLegalMove(coord, pieces)
+    def isLegalMove(self, coord, game):
+        legal = super().isLegalMove(coord, game)
         if not legal:
             return False
         move_x = (coord.x - self.currentCoordinate.x) 
@@ -253,79 +314,83 @@ class wPawn(Pawn):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_plt60.png")
-        self.team_color = "white"
+        self.team_color = Color.white
         
     def pawnMoveDirection(self):
+        return 1
+    def startingRow(self):
         return 1
     
 class wKing(King):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_klt60.png")
-        self.team_color = "white"
+        self.team_color = Color.white
         
 class wQueen(Queen):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_qlt60.png")
-        self.team_color = "white"
+        self.team_color = Color.white
         
 class wRook(Rook):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_rlt60.png")
-        self.team_color = "white"
+        self.team_color = Color.white
         
 class wBishop(Bishop):
     def __init__(self, startPosition, board):
        super().__init__(startPosition, board)
        self.image = pygame.image.load("resources/pieces/Chess_blt60.png")
-       self.team_color = "white"
+       self.team_color = Color.white
         
 class wKnight(Knight):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_nlt60.png")
-        self.team_color = "white"
+        self.team_color = Color.white
         
-class bPawn(Piece):
+class bPawn(Pawn):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_pdt60.png")
-        self.team_color = "black"
+        self.team_color = Color.black
         
     def pawnMoveDirection(self):
         return -1
+    def startingRow(self):
+        return 6
         
 class bKing(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_kdt60.png")
-        self.team_color = "black"
+        self.team_color = Color.black
         
 class bQueen(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_qdt60.png")
-        self.team_color = "black"
+        self.team_color = Color.black
         
 class bRook(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_rdt60.png")
-        self.team_color = "black"
+        self.team_color = Color.black
         
 class bBishop(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_bdt60.png")
-        self.team_color = "black"
+        self.team_color = Color.black
         
 class bKnight(Piece):
     def __init__(self, startPosition, board):
         super().__init__(startPosition, board)
         self.image = pygame.image.load("resources/pieces/Chess_kdt60.png")
-        self.team_color = "black"
+        self.team_color = Color.black
 
       
 if __name__ =="__main__":
@@ -341,22 +406,9 @@ if __name__ =="__main__":
         
     board = Board(SCREEN_HEIGHT)
     
-    wPieces = []
-    for i in np.arange(8):
-        wPieces.append(wPawn(Coordinate(1,i), board))
-    wPieces.append(wRook(Coordinate(0,0), board))
-    wPieces.append(wRook(Coordinate(0,7), board))
-    wPieces.append(wKnight(Coordinate(0,1), board))
-    wPieces.append(wKnight(Coordinate(0,6), board))
-    wPieces.append(wBishop(Coordinate(0,2), board))
-    wPieces.append(wBishop(Coordinate(0,5), board))
-    wPieces.append(wQueen(Coordinate(0,3), board))
-    wPieces.append(wKing(Coordinate(0,4), board))
-    
-    pieces = wPieces    
-    
+    game = Game()
     piece_is_moving = False
-    moving_piece = pieces[0]
+    moving_piece = game.pieces[0]
     # Run until the user asks to quit
     running = True
     while running:
@@ -367,7 +419,7 @@ if __name__ =="__main__":
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
               # get a list of all sprites that are under the mouse cursor
-              clicked_pieces = [s for s in pieces if s.rect.collidepoint(pygame.mouse.get_pos())]
+              clicked_pieces = [s for s in game.pieces if s.rect.collidepoint(pygame.mouse.get_pos())]
               
               assert len(clicked_pieces)< 2
               if len(clicked_pieces) == 1:
@@ -382,13 +434,13 @@ if __name__ =="__main__":
                   if(piece_is_moving):
                       [pos_x, pos_y] = pygame.mouse.get_pos()
                       final_coord = board.getCoordinate(PixPos(pos_x, pos_y))
-                      move(moving_piece, pieces,final_coord)
+                      game.move(moving_piece, final_coord)
                       piece_is_moving = False
 
         # Fill the background with white
         screen.fill((255, 255, 255))
         board.draw(screen)
-        for piece in pieces:
+        for piece in game.pieces:
             screen.blit(piece.image, piece.rect)
         # Flip the display
         pygame.display.flip()
